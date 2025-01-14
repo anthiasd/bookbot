1a50c7d854b161100855dd189ef60173f519a805
AnthiasD <mhladas@gmail.com>
2025-01-13 21:12:37 -0500

    character report in occurance order complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def open_book(path_to_file="./books/Frankenstein.txt"):
     with open(path_to_file) as f:
         contents = f.read()
-        return contents
+        return contents, path_to_file
     
 def count_words(book):
     return len(book.split())
@@ -22,25 +22,33 @@ def sort_dict(dict):
     for char in dict:
         listed.append({"char":char, "count":dict[char]})
     #listed.sort(reverse=True, key=listed["count"])
-    print(f"\n\n\n{listed}")
-    return None
+    listed.sort(reverse=True,key=sort_key)
+    #print(f"\n\n\n{listed}")
+    return listed
+
+def sort_key(chars):
+    return chars["count"]
 
 
 def main():
     print("Provide book filepath. 'Enter' for Frankenstein")
+    path = None
     path = input()
     if path == '':
-        book = open_book()
+        book, path = open_book()
     else:
         book = open_book(path)
 
+    print(f"=========================\nThis is the book report for {path.split("/")[-1:]}\n=========================")
     print(f"There are {count_words(book)} words in the book")
 
 
     chars = count_chars(book)
-    print("The character report:")
-    print(f"The character \"{chars}")
-
-    sort_dict(chars)
+    char_report = sort_dict(chars)
+    #print(char_report[0]["count"])
+    for i in range(0, len(char_report)):
+        print(f"The character '{char_report[i]["char"]}' has {char_report[i]["count"]} occurances.")
+    print("==================\nEnd of book report\n==================")
+    
 
 main()
